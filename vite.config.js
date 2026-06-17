2431822b40470ad0fd065790f6d1aed0a20fa42a
Robbe Wulgaert <59528716+RobbeW@users.noreply.github.com>
2026-06-17 16:36:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { cp } from "node:fs/promises";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/skore_analyser/",
   plugins: [
     react(),
     {
